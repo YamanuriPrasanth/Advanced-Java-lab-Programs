ee1a10c6cd59f66e8dc71b7df3f0f5ac4f0f1b6a
prasanth8745 <54884765+prasanth8745@users.noreply.github.com>
2022-01-03 18:41:01 +0530

    Update ControllerServlet.java

--- lab_program_13/ControllerServlet.java
+++ lab_program_13/ControllerServlet.java
@@ -8,7 +8,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import mypack.LoginBean;
 @WebServlet("/ControllerServlet")
-public class ControllerServlet extends HttpServlet {
+public class ControllerServlet extends HttpServlet
+{
     private static final long serialVersionUID = 1L;
     public ControllerServlet() 
     {
