1b51e02dab718bfc68ca8c7cd381eb887083365c
prasanth8745 <54884765+prasanth8745@users.noreply.github.com>
2022-01-03 19:14:34 +0530

    Create PieC.java

---
+++ lab_program_4/PieC.java
@@ -0,0 +1,23 @@
+import java.awt.*;
+import java.io.*;
+public class PieC
+{
+    private double data[ ];
+    public double getData(int index)
+    {
+        return data[index];
+    }
+    public void setData(int index, double value)
+    {
+        data[index] = value;
+    }
+    public double[ ] getData( )
+    {
+        return data;
+    }
+    public void setData(double[ ] values)
+    {
+        data = new double[values.length];
+        System.arraycopy(values, 0, data, 0, values.length);
+    }
+}
