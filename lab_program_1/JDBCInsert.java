72b4232b82c1966405231c3321fbbf759733ffa5
prasanth8745 <54884765+prasanth8745@users.noreply.github.com>
2022-01-25 08:57:59 +0530

    Update JDBCInsert.java

--- lab_program_1/JDBCInsert.java
+++ lab_program_1/JDBCInsert.java
@@ -7,7 +7,7 @@ public class JDBCInsert {
 		try
 		{
 			Class.forName("org.h2.Driver");
-			con=DriverManager.getConnection("jdbc:h2:tcp://localhost/~/test", "sa", "SA");
+			con=DriverManager.getConnection("jdbc:h2:tcp://localhost/~/test", "sa", "sa");
 			Statement st=con.createStatement();
 			int n=st.executeUpdate("insert into Employee values(101,'pqr',23,5,'chennai',230,460,2),(102,'iqr',22,3,'Mumbai',220,350,35);");
 		}
